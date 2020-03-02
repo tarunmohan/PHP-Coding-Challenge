4508e265fea932cb80e7c5b5a2e9f3cf32d16ac4
Tarun Kanagala <tarun@banyonhub.us>
2020-03-02 15:13:54 -0600

    Commit 4: License commands; finish the two incomplete commands; one to list all the caregivers with a license to the console; second email all the soon to expire caregivers using the Mailable and print the successfully sent emails to the console

--- routes/console.php
+++ routes/console.php
@@ -20,13 +20,10 @@ use Illuminate\Support\Facades\Mail;
  * List all skilled nurse license details.
  */
 Artisan::command('licenses:list', function () {
-    $headers = ['License Owner', 'License Expiry Date'];
-    $caregivers = [
-        ['Rosalind Johnston', '2020-01-13'],
-        ['Celestino Brekke', '2020-01-15'],
-        ['Willie Conroy', '2020-01-18'],
-    ];
-
+    //list all caregivers with a licence to the console
+    // Requirement was to list license detail, so I assume we also wanted the license number on the screen
+    $headers = ['License Owner', 'License Expiry Date', 'License Number'];
+    $caregivers = Caregiver::whereNotNull('license_number')->get(['name','license_expiration', 'license_number']);
     $this->table($headers, $caregivers);
 
 })->describe('List all skilled nurse license details');
@@ -36,8 +33,18 @@ Artisan::command('licenses:list', function () {
  * Notify nurses with licenses expiring soon.
  */
 Artisan::command('licenses:notify-expiring-soon', function () {
-    //
+    //query all caregivers not just skilled nurses for license expiration
+    $caregivers = Caregiver::whereNotNull('email')
+            ->whereRaw('DATEDIFF(license_expiration,now()) < ?')
+            ->setBindings([config('caregivers.license_renewal_reminder_in_days')])
+            ->get();
 
-    $this->info('Done');
-
+    foreach($caregivers as $caregiver){
+        try{
+            Mail::to($caregiver->email)->send(new LicenseExpiring($caregiver));
+            $this->info('Emailed ' . $caregiver->name); //logging each successfull caregiver to console
+        }catch(\Exception $e){
+            $this->info('Email to: ' . $caregiver->name . ' was not sent successfully'); //on error log to console
+        }
+    }
 })->describe('Notify nurses with licenses expiring soon');
